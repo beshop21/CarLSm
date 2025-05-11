f3d038a4fd4fd88e3ca6b4a44b2903c5cf8ba077
mario <cabdicasiisismail51@gmail.com>
2025-05-11 04:49:55 -0700

    Update for persons

--- BusinessLayer/ClsPersons.cs
+++ BusinessLayer/ClsPersons.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,35 @@ namespace BusinessLayer
     public class CLsPesrons
     {
 
+       public enum EnMode { addnew=0,update=1}
+      public  EnMode Mode = EnMode.addnew;
+        int personID { set; get; }
+        string FirstName { set; get; }
+        string SecondName { set; get; }
+        string NationID { set; get; }
+        DateTime DateOFBirth { set; get; }
+        string Email { set; get; }
+        string Phone { set; get; }
+        string Address { set; get; }
+        string ImagePath { set; get; }
+        int gendor { set; get; }
+        int CountryID { set; get; }
+        clsCountrys CountryInfo;
 
+       public CLsPesrons()
+        {
+            this.personID = -1;
+            this.FirstName = "";
+            this.SecondName = "";
+            this.NationID = "";
+            this.DateOFBirth = DateTime.Now;
+            this.Email = "";
+            this.Phone = "";
+            this.Address = "";
+            this.ImagePath = "";
+            this.gendor = -1;
+            this.CountryID = -1;
+        } 
 
     }
 }
